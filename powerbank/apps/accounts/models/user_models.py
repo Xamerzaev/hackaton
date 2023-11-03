ccef83bc1547905ea0d4d5c00525ecc207cecb43
Ramzan <ramzanxam@gmail.com>
2023-11-03 18:26:24 +0300

    recom model

--- powerbank/apps/accounts/models/user_models.py
+++ powerbank/apps/accounts/models/user_models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from powerbank.apps.event.models.event_models import Event
 
 class UserManager(BaseUserManager):
     use_in_migrations = True
@@ -70,8 +71,8 @@ class User(AbstractUser):
         choices=AccountType.choices, default=AccountType.GENERAL, max_length=8
     )
     image = models.ImageField(upload_to='user_images/', blank=True)
-    history_event = models.ManyToManyField()
-    favorite_event = models.ManyToManyField()
+    history_event = models.ManyToManyField(Event)
+    favorite_event = models.ManyToManyField(Event)
     first_name = None
     last_name = None
 
