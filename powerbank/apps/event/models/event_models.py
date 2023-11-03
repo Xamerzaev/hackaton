72b4fb415f2b684ea255bfc607f59079ea0e538c
mahamerz <mansur.ham@mail.ru>
2023-11-03 18:26:52 +0300

    optimise the models

--- powerbank/apps/event/models/event_models.py
+++ powerbank/apps/event/models/event_models.py
@@ -7,15 +7,14 @@ User = settings.AUTH_USER_MODEL
 
 class Event(models.Model):
     name = models.CharField(max_length=256)
-    type = models.CharField(max_length=128)
+    event_type = models.CharField(max_length=128)
     description = models.TextField()
     start = models.DateTimeField()
     location = models.PointField()
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='event_images')
-    reviews = models.OneToOneField
-    craeted_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class Review(models.Model):
@@ -29,10 +28,10 @@ class Review(models.Model):
 
 class Recommendation(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    event = models.ManyToManyField(Event)
+    events = models.ManyToManyField(Event)
 
 
 class Participation(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
-    status = models.CharField(verbose_name='Статус заявки')
+    status = models.CharField(max_length=128)
